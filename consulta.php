33274c693c68438310a7d082cf14d40164b68dfa
PrplHazeuwu <albertojr128@gmail.com>
2024-10-06 17:17:54 -0600

    añadí consulta global y eso

---
+++ consulta.php
@@ -0,0 +1,50 @@
+<?php include "Static/connect/db.php" ?>
+<?php include 'includes/header.php'?>
+
+<!-- Agregar estilo para centrar la tabla -->
+<style>
+    .centered-table {
+        margin: 0 auto; /* Centra la tabla horizontalmente */
+        text-align: center; /* Alinea el contenido de la tabla al centro */
+    }
+</style>
+
+    <table class = "table-primary centered-table">
+        <thead>
+            <TR>
+                <TH>Servicio</TH>
+                <TH>Costo</TH>
+            </TR>
+        </thead>
+
+<?php
+
+    $sql = "select * from servicios;";
+    $exec = mysqli_query($conn, $sql);
+
+    // Ciclo para recorrer fila por fila los resultados
+    while($rows=mysqli_fetch_array($exec)){
+?>    
+    
+        <tr>
+
+            <th><?php echo $rows['nombre']; ?></th>
+            <th><?php echo $rows['precio']; ?></th>
+        </tr>
+
+<?php    
+    }
+?>
+
+    </table>
+
+<br><br><br>
+
+<p>¿Te interesa lo que estás viendo? <a href="registro.php" style="color:blue;">regístrate aquí</a> y agenda una cita.</p>
+
+<a href="index.php" class="enlace">
+    <img src="./Static/img/back.png">
+    <p>Regresar</p>
+</a>
+
+<?php include 'includes/footer.php'?>
